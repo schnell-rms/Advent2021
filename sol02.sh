ab57eda6b54072f37ecc6c68e529bb3e1001945c
Remus Schnell <schnell.remus@gmail.com>
2025-03-22 13:35:21 +0100

    Day2: part 1

---
+++ sol02.sh
@@ -0,0 +1,74 @@
+#! /usr/bin/env bash
+
+sol02() {
+    if [[ ! -e $1 ]]
+    then
+        printf "Input file $red $1 $stop not found"
+        return 1
+    fi
+
+    declare -i x=0
+    declare -i y=0
+
+    while read line || [[ -n $line ]]
+    do
+        # echo $line
+        if [[ $line =~ forward\ ([0-9]+) ]]
+        then
+            (( x += ${BASH_REMATCH[1]} ))
+        elif [[ $line =~ down\ ([0-9]+) ]]
+        then
+            (( y += ${BASH_REMATCH[1]} ))
+        elif [[ $line =~ up\ ([0-9]+) ]]
+        then
+            (( y -= ${BASH_REMATCH[1]} ))
+        else
+            printf "Unexpected line '$red$line$stop'\n"
+        fi
+    done < $1
+
+    printf "Forward: $x\n"
+    printf "Depth: %d\n" $y
+    printf "Key %d\n" $(($x * $y))
+    return 0
+}
+
+main() {
+
+    printf "Running $0\n"
+
+    red="\033[31m"
+    stop="\033[0m"
+
+    use_sample_input=false
+    while getopts :s option
+    do
+        case $option in
+            s) use_sample_input=true;;
+            ?) echo -e "Unknown option $red'$OPTARG'$stop";;
+        esac
+    done
+
+    file_path=$(pwd)
+    file_path+="/inputs/"
+
+    # Just for the sake of exercising:
+    if [[ $0 =~ ([0-9]+).sh ]]
+    then
+        file_path+=${BASH_REMATCH[1]}
+    else
+        echo "Could not determine the input file based on the script's name"
+        exit
+    fi
+
+    if $use_sample_input
+    then
+        file_path+="Sample"
+    fi
+
+    file_path+=".txt"
+
+    sol02 $file_path
+}
+
+main $@
