4cfd2cae7669208f57a72ff53a0e6998821b23a4
Remus Schnell <schnell.remus@gmail.com>
2025-04-18 00:51:51 +0200

    Day05: second star

--- sol05.py
+++ sol05.py
@@ -4,8 +4,7 @@ import numpy as np
 from collections import defaultdict
 
 
-def main(inputPath):
-
+def sol1(inputPath):
     map=defaultdict(int)
 
     def checkLine(line):
@@ -23,5 +22,36 @@ def main(inputPath):
     firstStar = sum([map[x] > 1 for x in map.keys()])
     print("First star: ", firstStar)
 
+def sol2(inputPath):
+    map=defaultdict(int)
+
+    def checkLine(line):
+        numbers = np.array([int(x) for x in re.findall(r"\d+", line)])
+        step_row = 1 if numbers[0]<numbers[2] else 0 if numbers[0]==numbers[2] else -1
+        step_col = 1 if numbers[1]<numbers[3] else 0 if numbers[1]==numbers[3] else -1
+        assert(numbers[0]==numbers[2] or numbers[1] == numbers[3] or abs(numbers[0]-numbers[2]) == abs(numbers[1]-numbers[3]))
+
+        i = numbers[0]
+        j = numbers[1]
+
+        while (i != numbers[2] or j != numbers[3]):
+            map[(i,j)] += 1
+            # print(i, " <> ",j)
+            i += step_row
+            j += step_col
+
+        map[(i,j)] += 1
+        # print(i, " <> ",j)
+        assert(j==numbers[3])
+
+    with open(inputPath) as file:
+        for line in file:
+            checkLine(line)
+
+    secondStar = sum([map[x] > 1 for x in map.keys()])
+    print("Second star: ", secondStar)
+
 if __name__=="__main__":
-    main("inputs/05.txt")
+    finput = "inputs/05.txt"
+    sol1(finput)
+    sol2(finput)
